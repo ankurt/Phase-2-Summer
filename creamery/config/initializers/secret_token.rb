cc3f6c440de6bad2091fc411addbec31ea53b1f6
Ankur Toshniwal <ankur@getfbpay.in>
2014-07-16 20:14:27 -0400

    rails app started and scaffolding complete

---
+++ creamery/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Creamery::Application.config.secret_key_base = 'f283b3f3de9389d28f335f9e90a4d88707b283d7cf07262670397e569127c7f22ceb14d44d4a084151d56a58a591f8022b815b33c3a1e0a501dd6540af90a875'
